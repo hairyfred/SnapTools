72620d04c2e32b68c635190c4ee4b59d22f0fd6c
haydhook <ID626493@outlook.com>
2020-05-29 15:10:54 +0100

    Remove account manager

--- app/src/pack/java/com/ljmu/andre/snaptools/ModulePack/ModulesDef.java
+++ app/src/pack/java/com/ljmu/andre/snaptools/ModulePack/ModulesDef.java
@@ -122,17 +122,10 @@ class ModulesDef extends ConstantDefiner<Modules> {
 
     // ===========================================================================
 
-    public static final Modules ACCOUNT_MANAGER = new Modules(
-            13,
-            "Account Manager",
-            AccountManager.class,
-            "Allows for safely and securely swapping between Snapchat accounts, along with other account management features"
-    );
-
     // ===========================================================================
 
     public static final Modules FORCED_HOOKS = new Modules(
-            14,
+            13,
             "Forced Hooks",
             ForcedHooks.class,
             false
